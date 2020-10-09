c2f03df9128e9ea265a866ed932f8802a255e415
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-10-09 02:09:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,22 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
+html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
+doc = Nokogiri::HTML(html)
+
+courses = 
+
 class Scraper
+  
+  def get_page 
+    html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
+    doc = Nokogiri::HTML(html)
+    binding.pry
+  end
+  
   
   def print_courses
     self.make_courses
